56e5b2f018e6519a0a49f7a98cdb5cb7d877916b
Cornelia1991 <Cornelia1991@users.noreply.github.com>
2020-05-21 18:34:57 -0400

    get request to unsplash

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,17 @@
 import React from "react";
+import axios from "axios";
 import SearchBar from "./SearchBar";
 
 class App extends React.Component {
-  onSearchSubmit(term) {
-    console.log(term);
+  async onSearchSubmit(term) {
+    const response = await axios.get("https://api.unsplash.com/search/photos", {
+      params: { query: term },
+      headers: {
+        Authorization: "Client-ID bdn6njZFsvq_J5NlUe_mLYwXcQtacVEWkt22FLo4qtI",
+      },
+    });
+
+    console.log(response.data.results);
   }
 
   render() {
